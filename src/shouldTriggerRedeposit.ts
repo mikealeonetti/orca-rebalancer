4c4402157598e0f4ebd2c354ab20aa209fdaedf3
Michael A. Leonetti <mikealeonetti@gmail.com>
2024-03-28 19:08:40 -0500

    Added a daily redeposit trigger.

--- src/shouldTriggerRedeposit.ts
+++ src/shouldTriggerRedeposit.ts
@@ -16,7 +16,7 @@ export default async function (positions: WhirlpoolPositionInfo[]) {
 
         const publicKeyString = position.publicKey.toString();
 
-        debug( "Checking %s to see if we need to re-deposit.", publicKeyString);
+        debug("Checking %s to see if we need to re-deposit.", publicKeyString);
 
         // Can we get the history?
         const [
@@ -45,34 +45,53 @@ export default async function (positions: WhirlpoolPositionInfo[]) {
 
         debug("totalFeesInUSDC=%s, enteredPriceUSDC=%s", totalFeesInUSDC, enteredPriceUSDC);
 
+        // Now
+        const now = new Date();
+
         // Get what percent of the opening price we have in feez now
         const percentOfOpeningPrice = totalFeesInUSDC.div(enteredPriceUSDC).times(100);
+        const lastRewardsCollected = dbWhirlpool.lastRewardsCollected || dbWhirlpool.createdAt || now;
+
+        const millisSinceRewardsCollected = now.valueOf() - lastRewardsCollected.valueOf();
+        const hoursSinceLastRewardsCollected = millisSinceRewardsCollected / (60 * 60 * 1000);
 
         debug("percentOfOpeningPrice=%s", percentOfOpeningPrice);
 
+        debug("lastRewardsCollected=%s, millisSinceRewardsCollected=%s, hoursSinceLastRewardsCollected=%s", lastRewardsCollected, millisSinceRewardsCollected, hoursSinceLastRewardsCollected);
+
+        // Should we
+        let shouldTriggerRedeposit = false;
+
+        // Do we have a trigger?
+        if (percentOfOpeningPrice.gte(1)) {
+            logger.info("Percent of opening price is greater than 1%. Trigger re-deposit.");
+            shouldTriggerRedeposit = true;
+        }
+        else if (hoursSinceLastRewardsCollected >= 24) {
+            logger.info("Over 24 hours since last re-deposit. Trigger re-deposit.");
+            shouldTriggerRedeposit = true;
+        }
+
         // Is it greater than 1%?
-        if( percentOfOpeningPrice.gte(1) ) {
-            logger.info( "Percent of opening price is greater than 1%. Trigger re-deposit." );
-    
-
+        if (shouldTriggerRedeposit) {
             // First trigger collection
-            logger.debug( "Collecting rewards." );
+            logger.debug("Collecting rewards.");
 
-            await collectFees( position );
+            await collectFees(position);
 
             // Now redeposit
-            logger.debug( "Re-depositing." );
+            logger.debug("Re-depositing.");
 
             // Update to trigger forcing redeposits
-            await DBWhirlpool.update( {
-                redepositAttemptsRemaining : 10
+            await DBWhirlpool.update({
+                redepositAttemptsRemaining: 10
             },
-            {
-                where : { publicKey : position.publicKey.toString() }
-            });
+                {
+                    where: { publicKey: position.publicKey.toString() }
+                });
         }
         else {
-            debug( "Not enough to want to re-deposit." );
+            debug("No re-deposit conditions met.");
         }
     }
 }
